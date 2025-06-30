3c6a9441f7411d1d5ac7c1c24707d0ab8706a146
Gaël Louage <gaellouage@hotmail.com>
2025-06-30 13:14:38 +0200

    +1 array exercise

---
+++ ArrayPlusOneEx/Program.cs
@@ -0,0 +1,29 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var num = new int[] { 0, 4, 2 };
+        var upArray = UpArray(num);
+        foreach (var n in upArray)
+        {
+            Console.WriteLine(n);
+          
+        }
+    }
+    public static int[] UpArray(int[] num)
+    {
+        if(num.Any( x => x < 0) || num.Any(x => x > 9)) return null;
+
+        
+        var plusOneResult = int.Parse(string.Concat(num)) + 1;
+        var plusToToString = plusOneResult.ToString();
+        var result = new List<int>();
+
+        for(int i = 0; i< plusToToString.Length; i++)
+        {
+            result.Add((int)char.GetNumericValue(plusToToString[i]));
+        }
+
+        return result.ToArray();
+    }
+}
